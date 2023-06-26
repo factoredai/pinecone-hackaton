6acc1c245af22f5a6e8e2943af18ff16a65ad184
Mateo Restrepo <mateorestrepo@factored.ai>
2023-06-25 19:07:31 -0500

    Read config file with config parser

--- frontend/demo.py
+++ frontend/demo.py
@@ -1,28 +1,26 @@
-from langchain.embeddings.huggingface import HuggingFaceInstructEmbeddings
-from langchain.vectorstores.pinecone import Pinecone
+import os
+from configparser import ConfigParser
+
+import chainlit as cl
+import pinecone
+from dotenv import load_dotenv
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
+from langchain.embeddings import CohereEmbeddings
+from langchain.embeddings.base import Embeddings
+from langchain.embeddings.huggingface import (
+    HuggingFaceEmbeddings,
+    HuggingFaceInstructEmbeddings,
 )
 from langchain.memory import ConversationBufferMemory
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-
-from langchain.callbacks.streaming_aiter import AsyncIteratorCallbackHandler
+from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
+from langchain.vectorstores.pinecone import Pinecone
 
 from prompts import system_template
 
-import os
-from dotenv import load_dotenv
-import pinecone
-import chainlit as cl
-
 load_dotenv()
-pinecone.init(api_key=os.getenv("PINECONE_API_KEY"),environment=os.getenv("PINECONE_ENV"))
-openai_api_key = os.environ["OPENAI_API_KEY"] 
-
+pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENV"))
+openai_api_key = os.environ["OPENAI_API_KEY"]
 
 messages = [
     SystemMessagePromptTemplate.from_template(system_template),
@@ -31,65 +29,93 @@ messages = [
 prompt = ChatPromptTemplate.from_messages(messages)
 chain_type_kwargs = {"prompt": prompt}
 
+
+def initialize_embeddings():
+    cfg = ConfigParser()
+    cfg.read("./frontend/config.conf")
+
+    if cfg.get("embeddings", "type") == "instruct":
+        embeddings = HuggingFaceInstructEmbeddings(model_name=cfg.get("instruct_embeddings", "model"))
+    elif cfg.get("embeddings", "type") == "sentence_transformers":
+        embeddings = HuggingFaceEmbeddings(model_name=cfg.get("sentence_transformers_embeddings", "model"))
+    elif cfg.get("embeddings", "type") == "cohere":
+        print("Using Cohere embeddings")
+        embeddings = CohereEmbeddings(
+            cohere_api_key=os.environ.get("COHERE_API_KEY"),
+            model=cfg.get("cohere_embeddings", "model"),
+            truncate=cfg.get("cohere_embeddings", "truncate"),
+        )
+    else:
+        raise NotImplementedError()
+    return embeddings
+
+
 @cl.action_callback("action_button")
 async def on_action(action):
     actions = cl.user_session.get("actions")
     cl.user_session.set("task", action.value)
     if action.value == "prior_art":
-        await cl.Message(content="You chose prior art search. What are you looking for?", author = 'Librarian').send()
+        await cl.Message(content="You chose prior art search. What are you looking for?", author="Librarian").send()
         cl.user_session.set("botname", "Librarian")
     elif action.value == "draft_patent":
-        await cl.Message(content="You chose to draft a patent. What is your invention?", author = 'Drafter').send()
+        await cl.Message(content="You chose to draft a patent. What is your invention?", author="Drafter").send()
         cl.user_session.set("botname", "Drafter")
     elif action.value == "compare_patent":
-        await cl.Message(content="You chose to compare your patent. What is your patent?", author = 'Curator').send()
+        await cl.Message(content="You chose to compare your patent. What is your patent?", author="Curator").send()
         cl.user_session.set("botname", "Curator")
-                    
+
     for action in actions:
         await action.remove()
     # Optionally remove the action button from the chatbot user interface
-    #await action.remove()
+    # await action.remove()
+
 
 @cl.on_chat_start
 async def start():
     # Sending an action button within a chatbot message
     actions = [
-        cl.Action(name="action_button", value='prior_art', description="Click me!", label = "Prior art search"),
-        cl.Action(name="action_button", value='draft_patent', description="Click me!", label = "Draft your patent"),
-        cl.Action(name="action_button", value='compare_patent', description="Click me!", label = "Compare your patent")]
+        cl.Action(name="action_button", value="prior_art", description="Click me!", label="Prior art search"),
+        cl.Action(name="action_button", value="draft_patent", description="Click me!", label="Draft your patent"),
+        cl.Action(name="action_button", value="compare_patent", description="Click me!", label="Compare your patent"),
+    ]
     cl.user_session.set("actions", actions)
 
     await cl.Message(content="How can PatentBot help you today?", actions=actions).send()
+
 
 @cl.on_message
 async def main(message: str):
     # Your custom logic goes here...
     task = cl.user_session.get("task")
-    chain = await get_model(task)
+
+    embeddings = initialize_embeddings()
+    chain = await get_model(task, embeddings)
+
     res = await cl.make_async(chain)(message, callbacks=[cl.ChainlitCallbackHandler()])
-    # msg = cl.Message("")
-    # async for stream_resp in cl.user_session.get('async_iterator').aiter():
-    #     token = stream_resp.get("choices")[0].get("text")
-    #     await msg.stream_token(token)
-    # await msg.send()
-    msg = cl.Message(content="", author = cl.user_session.get("botname"))
+
+    msg = cl.Message(content="", author=cl.user_session.get("botname"))
     for token in res["answer"].split(" "):
         await cl.sleep(0.1)
         await msg.stream_token(token + " ")
     await msg.send()
-    #await cl.Message(content=res["answer"], author = cl.user_session.get("botname")).send()
 
-async def get_model(task: str):
+
+async def get_model(task: str, embeddings: Embeddings):
+    cfg = ConfigParser()
+    cfg.read("./frontend/config.conf")
+
     if cl.user_session.get("chain"):
         return cl.user_session.get("chain")
-    
-    embeddings = HuggingFaceInstructEmbeddings()
+
     docsearch = await cl.make_async(Pinecone.from_existing_index)(
-        'second-test', embeddings
+        index_name=cfg.get("pinecone", "index_name"),
+        embedding=embeddings,
     )
+
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
     chain = ConversationalRetrievalChain.from_llm(
-        ChatOpenAI(temperature=0, streaming = True, openai_api_key=openai_api_key),
+        ChatOpenAI(temperature=0, streaming=True, openai_api_key=openai_api_key),
         chain_type="stuff",
         retriever=docsearch.as_retriever(),
         memory=memory,
